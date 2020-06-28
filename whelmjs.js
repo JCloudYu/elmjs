f4bee7155c7195914da48451d1aaf632a0dc6183
Cloud Yu <j.cloud.yu@gmail.com>
2020-06-28 14:27:57 +0800

    Add WhelmJS::BindTextMap, WhelmJS::UnbindTextMap, and WhelmJS::UseTextMapGroup apis that allow developers to perform simple fast text mapping among elements

--- whelmjs.js
+++ whelmjs.js
@@ -7,6 +7,7 @@
 	
 	const _VERSION		= "1.0.11";
 	const _EVENT_FORMAT = /^((bubble::)?[a-zA-Z0-9\-_ ]+::[a-zA-Z0-9\-_ ]+)(,([a-zA-Z0-9\-_ ]+::[a-zA-Z0-9\-_ ]+))*$/;
+	const _MAP_TEXT_FORMAT = /^([a-zA-Z0-9\-_]+(::[a-zA-Z0-9\-_]+)?)(,([a-zA-Z0-9\-_]+(::[a-zA-Z0-9\-_]+)?))*$/;
 	const _PRIVATES		= new WeakMap();
 	const _EVENT_MAP	= new WeakMap();
 	const _INST_MAP		= new WeakMap();
@@ -99,17 +100,77 @@
 		
 		_CONTROLLERS.set(name, info);
 	};
-	ELM_JS_ENDPOINT.controller = (name, controller, is_constructor=true)=>{
+	ELM_JS_ENDPOINT.GetInstance = (element)=>{
+		return _INST_MAP.get(element)||null;
+	};
+	
+	
+	const _TEXT_MAPPER = Object.create(null);
+	_TEXT_MAPPER['_default'] = [];
+	
+	let _TEXT_GROUP = '';
+	ELM_JS_ENDPOINT.UseTextMapGroup = (group)=>{
+		_TEXT_GROUP = (''+(group||'')).trim();
+	};
+	ELM_JS_ENDPOINT.BindTextMap = function(map, group='_default') {
+		if ( typeof group !== "string" ) {
+			throw new TypeError("Input map group should be a string identifier!");
+		}
+	
+		const is_map_func = typeof map === "function";
+		const is_map_obj  = Object(map) === map;
+		if ( !is_map_func && !is_map_obj ) {
+			throw new TypeError("Input map should be an object or a function!");
+		}
+		
+		const map_group = _TEXT_MAPPER[group] = _TEXT_MAPPER[group]||[];
+		const idx = map_group.indexOf(map);
+		if ( idx < 0 ) {
+			map_group.push(map);
+		}
+		
+		return ELM_JS_ENDPOINT;
+	};
+	ELM_JS_ENDPOINT.UnbindTextMap = function(map, group='_default') {
+		if ( typeof group !== "string" ) {
+			throw new TypeError("Input map group should be a string identifier!");
+		}
+	
+		const is_map_func = typeof map === "function";
+		const is_map_obj  = Object(map) === map;
+		if ( !is_map_func && !is_map_obj ) {
+			throw new TypeError("Input map should be an object or a function!");
+		}
+		
+		const map_group = _TEXT_MAPPER[group] = _TEXT_MAPPER[group]||[];
+		const idx = map_group.indexOf(map);
+		if ( idx >= 0 ) map_group.splice(idx, 1);
+		
+		return ELM_JS_ENDPOINT;
+	};
+	function ___MAP_TEXT(key) {
+		const group = _TEXT_MAPPER[_TEXT_GROUP]||_TEXT_MAPPER['_default'];
+		for(let i=group.length; i>0; i--) {
+			const map = group[i-1];
+			const map_result = (typeof map === "function") ? map(key) : map[key];
+			if ( map_result !== undefined ) {
+				return (''+(map_result||'')).trim();
+			}
+		}
+		
+		return key;
+	}
+	
+	
+	
+	
+	ELM_JS_ENDPOINT.controller = function(name, controller, is_constructor=true) {
 		console.warn("WhelmJS.controller is marked as deprecated! Please use WhelmJS.BindInst instead!");
 		return ELM_JS_ENDPOINT.BindInst(...Array.prototype.slice.call(arguments, 0));
 	};
-	ELM_JS_ENDPOINT.DefineBlueprint = (name, controller, is_constructor=true)=>{
+	ELM_JS_ENDPOINT.DefineBlueprint = function(name, controller, is_constructor=true) {
 		console.warn("WhelmJS.DefineBlueprint is marked as deprecated! Please use WhelmJS.BindInst instead!");
 		return ELM_JS_ENDPOINT.BindInst(...Array.prototype.slice.call(arguments, 0));
-	};
-	
-	ELM_JS_ENDPOINT.GetInstance = (element)=>{
-		return _INST_MAP.get(element)||null;
 	};
 	window.WhelmJS = Object.freeze(ELM_JS_ENDPOINT);
 	
@@ -274,7 +335,8 @@
 			const event_descriptor = item.getAttribute(bind_bubble_event ? 'elm-bind-event-bubble' : 'elm-bind-event').trim();
 			const matches = _EVENT_FORMAT.test(event_descriptor);
 			if ( !matches ) {
-				throw new SyntaxError(`Incorrect event '${event_descriptor}' in 'elm-bind-event' tag`);
+				console.error(item);
+				throw new SyntaxError(`Incorrect event '${event_descriptor}' in ${item.tagName}[elm-bind-event] attribute!`);
 			}
 			
 			
@@ -318,6 +380,29 @@
 				
 				item.addEventListener(source_event, event_dispatcher);
 				ITEM_EVENT_MAP.set(event_identifier, event_dispatcher)
+			}
+		}
+		
+		
+		
+		const should_map_text = item.hasAttribute('elm-map-text');
+		if ( should_map_text ) {
+			const map_text = item.getAttribute('elm-map-text');
+			if ( !_MAP_TEXT_FORMAT.test(map_text) ) {
+				console.error(item);
+				throw new SyntaxError(`Given ${item.tagName}[elm-map-text] attribute contains invalid attribute!`);
+			}
+			
+			const map_list = map_text.trim().split(',').map((item)=>item.trim()).filter((item)=>item!=="");
+			for (const map of map_list) {
+				const [text, target_attr] = map.split('::').map((item)=>item.trim());
+				const mapped_text = ___MAP_TEXT(text);
+				if (target_attr) {
+					item.setAttribute(target_attr, mapped_text);
+				}
+				else {
+					item.textContent = mapped_text;
+				}
 			}
 		}
 	}
